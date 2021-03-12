574f3aefddb97b915101e6b24bdc9b4043784b47
Roudart <diego3_gallegos@hotmail.com>
2021-03-12 15:39:08 -0600

    Creación de proyecto
    Proyecto creado con nugets instalados y tablas creadas, con su contexto implementado

---
+++ SMedia/SMedia/Models/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SMedia.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public int SenderId { get; set; }
+        public int ReceiverId { get; set; }
+        public DateTime Date { get; set; }
+
+        public virtual User Sender { get; set; }
+        public virtual User Receiver { get; set; }
+    }
+}
