f3bbfa0355dd5e3c5ad5e540bb4bb737f04ac2e6
Axel Lara <axel.larazz@gmail.com>
2021-04-04 19:05:16 -0500

    agregada sombra de luz a la modal

--- woofer-frontend/src/App/shared_components/publisher/publisher.js
+++ woofer-frontend/src/App/shared_components/publisher/publisher.js
@@ -20,7 +20,7 @@ class Publisher extends React.Component{
     modalWindow(){
 
         return(
-        <section className={"flex flex-col bg-dark w-full h-full md:w-5/6 md:h-3/4 m-auto md:mt-40 text-white py-4"}>
+        <section className={"flex flex-col bg-dark w-full h-full md:w-5/6 md:h-3/4 m-auto md:mt-40 text-white py-4 shadow-innerW"}>
             <h3 className={"font-bold text-light text-xl text-center"}>Crear publicación</h3>
            <div className={"flex flex-row justify-between items-center mt-6 md:mt-2"}>
                 <button onClick={()=>this.toggleOpenModal()} className={"transform scale-150 ml-8 text-white "}>
