6f9b0a93f688bf11767e6c5f116de63a4c8e9c0f
Roudart <diego3_gallegos@hotmail.com>
2021-04-23 03:20:11 -0500

    Segunda parte del commit
    Commit para agregar los metodos request del segundo avance

--- SMedia/SMedia/Clases/Core/SMediaCore.cs
+++ SMedia/SMedia/Clases/Core/SMediaCore.cs
@@ -23,7 +23,7 @@ namespace SMedia.Clases.Core
                    from s in dbContext.Post
                    where s.Active == true
                    select s
-                   ).ToList();
+                   ).Take(10).ToList();
                 return Posts;
             }
             catch (Exception ex)
@@ -80,6 +80,27 @@ namespace SMedia.Clases.Core
                 throw ex;
             }
         }
+        public long Login(string NickName, string Password)
+        {
+            try
+            {
+                bool AnyUser = dbContext.User.Any(user => user.NickName == NickName && user.Password == Password);
+                if (AnyUser)
+                {
+                    User user = (
+                        from U in dbContext.User
+                        where (U.NickName == NickName && U.Password == Password)
+                        select U        
+                        ).First();
+                    return user.Id;
+                }
+                return -1;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
         public bool ValidateUser(User user)
         {
             try
@@ -123,7 +144,7 @@ namespace SMedia.Clases.Core
                                 join FC in CommIFollow on LP.CommunityId equals FC.CommunityId
                                 orderby LP.CreationDate
                                 select LP
-                                ).ToList();
+                                ).Take(10).ToList();
                             return LastPosts;
                         }
                         else { return null; }
@@ -138,6 +159,37 @@ namespace SMedia.Clases.Core
             }
         }
 
+        public List<Post> GetFavoritePosts(int id)
+        {
+            try
+            {
+                bool anyUser = dbContext.Post.Any(user => user.Id == id);
+                if (anyUser)
+                {
+                    bool anyFavorite = dbContext.FavoritePost.Any(fav => fav.UserId == id);
+                    if (anyFavorite)
+                    {
+                        var FavPost = from FP in dbContext.FavoritePost
+                                          where FP.UserId == id
+                                          select FP;
+                            List<Post> FavoritePosts = (
+                                from LP in dbContext.Post
+                                join FU in FavPost on LP.Id equals FU.PostId
+                                orderby LP.CreationDate
+                                select LP
+                                ).Take(10).ToList();
+                            return FavoritePosts;
+                    }
+                    else { return null; }
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
         public List<Message> GetMessages(int id)
         {
             try
@@ -147,7 +199,7 @@ namespace SMedia.Clases.Core
                 if (anyUser && anyMessage) {
                     List<Message> messages = (from M in dbContext.Message
                                              where (M.ReceiverId == id || M.SenderId == id)
-                                             select M).ToList();
+                                             select M).Take(20).ToList();
                     return messages;
                 }
                 return null;
@@ -157,21 +209,73 @@ namespace SMedia.Clases.Core
                 throw ex;
             }
         }
+        public int GetPostViewes(int id)
+        {
+            try
+            {
+                bool AnyPost = dbContext.Post.Any(post => post.Id == id);
+                if (AnyPost)
+                {
+                    IEnumerable<Viewed> viewsPost = ( from V in dbContext.Viewed
+                                                    where V.PostId == id
+                                                    select V);
+                    int viewes = viewsPost.Count();
+                    return viewes;
+                }
+                return -1;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public bool SetViewOnPost(int idUser, int idPost)
+        {
+            try
+            {
+                bool AnyUser = dbContext.User.Any(user => user.Id == idUser);
+                bool AnyPost = dbContext.Post.Any(post => post.Id == idPost);
+                if(AnyUser && AnyUser)
+                {
+                    bool anyViewed = dbContext.Viewed.Any(viewed => viewed.UserId == idUser
+                                                            && viewed.PostId == idPost);
+                    if (!anyViewed)
+                    {
+                        Viewed viewed = new Viewed
+                        {
+                            UserId = idUser,
+                            PostId = idPost
+                        };
+                        dbContext.Add(viewed);
+                        dbContext.SaveChanges();
+                        return true;
+                    }
+                    return false;
+                }
 
-        public void CreatePost(Post newpost)
+                return false;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool CreatePost(Post newpost)
         {
             try
             {
 
-                bool validUser = ValidatePost(newpost);
-                if (validUser)
+                bool validPost = ValidatePost(newpost);
+                if (validPost)
                 {
                     newpost.CreationDate = DateTime.Now;
                     newpost.Active = true;
                     dbContext.Add(newpost);
                     dbContext.SaveChanges();
+                    return true;
                 }
-
+                return false;
             }
             catch (Exception ex)
             {
@@ -179,11 +283,166 @@ namespace SMedia.Clases.Core
             }
         }
 
+        public bool SaveFavoritePost(int idUser, int idPost)
+        {
+            try
+            {
+                bool validUser = dbContext.User.Any(user => user.Id == idUser);
+                bool validPost = dbContext.Post.Any(post => post.Id == idPost);
+                if(validUser && validPost)
+                {
+                    bool anyPost = dbContext.FavoritePost.Any(favPost => favPost.UserId == idUser && favPost.PostId == idPost);
+                    if (!anyPost)
+                    {
+                        FavoritePost favPost = new FavoritePost
+                        {
+                            PostId = idPost,
+                            UserId = idUser
+                        };
+                        dbContext.Add(favPost);
+                        dbContext.SaveChanges();
+                        return true;
+                    }
+                    return false;
+                }
+                return false;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public List<User> GetFollowedUsers(int id)
+        {
+            try
+            {
+                bool anyFolloweUser = dbContext.FollowedUser.Any(follUser => follUser.FollowerId == id);
+                if (anyFolloweUser) {
+                    var UserIFollow = (from FU in dbContext.FollowedUser
+                                       where FU.FollowerId == id
+                                       select FU);
+                    List<User> FollowedUsers = (from U in dbContext.User
+                                                join FU in UserIFollow on U.Id equals FU.FollowedId
+                                                select U).ToList();
+                    return FollowedUsers;
+                }
+                return null;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public List<Community> GetFollowedCommunities(int id)
+        {
+            try
+            {
+                bool anyFollowedCommunity = dbContext.FollowedCommunity.Any(follComm => follComm.FollowerId == id);
+                if (anyFollowedCommunity)
+                {
+                    var communityIFollow = (from FU in dbContext.FollowedCommunity
+                                            where FU.FollowerId == id
+                                            select FU);
+                    List<Community> FollowedCommunities = (from U in dbContext.Community
+                                                join FU in communityIFollow on U.Id equals FU.CommunityId
+                                                select U).ToList();
+                    return FollowedCommunities;
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool FollowUser(int idFollower, int idToFollow)
+        {
+            try
+            {
+                bool anyFollowedUser = dbContext.FollowedUser.Any(follUser => follUser.FollowerId == idFollower 
+                                                                    && follUser.FollowedId == idToFollow);
+                if (!anyFollowedUser && idFollower != idToFollow)
+                {
+                    FollowedUser follUser = new FollowedUser
+                    {
+                        FollowerId = idFollower,
+                        FollowedId = idToFollow
+                    };
+                    follUser.DateOfFollow = DateTime.Now;
+                    dbContext.Add(follUser);
+                    dbContext.SaveChanges();
+                    return true;
+                }
+                return false;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool FollowCommunity(int idFollower, int idToFollow)
+        {
+            try
+            {
+                bool anyFollow = dbContext.FollowedCommunity.Any(follow => follow.FollowerId == idFollower
+                                                                    && follow.CommunityId == idToFollow);
+                bool AnyUser = dbContext.User.Any(user => user.Id == idFollower);
+                bool AnyCommunity = dbContext.Community.Any(community => community.Id == idToFollow);
+                if (!anyFollow && AnyUser && AnyCommunity)
+                {
+                    FollowedCommunity Follow = new FollowedCommunity
+                    {
+                        FollowerId = idFollower,
+                        CommunityId = idToFollow
+                    };
+                    Follow.DateOfFollow = DateTime.Now;
+                    dbContext.Add(Follow);
+                    dbContext.SaveChanges();
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool SetReactPost(int idPost, int idUser, byte typeReaction)
+        {
+            try
+            {
+                bool AnyReaction = dbContext.Reaction.Any(reaction => reaction.PostId == idPost
+                                                            && reaction.UserId == idUser);
+                bool AnyUser = dbContext.User.Any(user => user.Id == idUser);
+                bool AnyPost = dbContext.Post.Any(post => post.Id == idPost);
+                if (!AnyReaction && AnyUser && AnyPost)
+                {
+                    Reaction newReaction = new Reaction
+                    {
+                        UserId = idUser,
+                        PostId = idPost,
+                        Type = typeReaction
+                    };
+                    dbContext.Add(newReaction);
+                    dbContext.SaveChanges();
+                    return true;
+                }
+                return false;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
         public void SendMessage(Message message)
         {
             try
             {
-
                 bool validUser = ValidateMessage(message);
                 if (validUser)
                 {
@@ -191,9 +450,29 @@ namespace SMedia.Clases.Core
                     dbContext.Add(message);
                     dbContext.SaveChanges();
                 }
-
             }
             catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool CreateCommunity(Community community)
+        {
+            try
+            {
+                bool validCommunity = ValidateCommunity(community);
+                if (validCommunity)
+                {
+                    community.CreationDate = DateTime.Now;
+                    community.Active = true;
+                    dbContext.Add(community);
+                    dbContext.SaveChanges();
+                    return true;
+                }
+                return false;
+            }
+            catch(Exception ex)
             {
                 throw ex;
             }
@@ -211,6 +490,28 @@ namespace SMedia.Clases.Core
                 return true;
             }
             catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public bool ValidateCommunity(Community community)
+        {
+            try
+            {
+                if(string.IsNullOrEmpty(community.Name) || string.IsNullOrEmpty(community.Color)
+                    || string.IsNullOrEmpty(community.Description) || community?.OwnerId == null || community.OwnerId < 1)
+                {
+                    return false;
+                }
+                else
+                {
+                    bool AnyUser = dbContext.User.Any(user => user.Id == community.OwnerId);
+                    if (!AnyUser)
+                        return false;
+                }
+                return true;
+            }
+            catch(Exception ex)
             {
                 throw ex;
             }
