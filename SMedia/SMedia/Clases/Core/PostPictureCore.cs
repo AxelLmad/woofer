49a818bc335a75611b2d7fd52aa46a3afdc45035
Axel Lara <axel.larazz@gmail.com>
2021-04-24 01:48:58 -0500

    get de postpicture

---
+++ SMedia/SMedia/Clases/Core/PostPictureCore.cs
@@ -0,0 +1,35 @@
+﻿using SMedia.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SMedia.Clases.Core
+{
+    public class PostPictureCore
+    {
+        SMediaDbContext dbContext;
+        public PostPictureCore(SMediaDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public List<PostPicture> GetByPostId(long id)
+        {
+            try
+            {
+                List<PostPicture> pictures = (
+                   from s in dbContext.PostPicture
+                   where s.PostId == id
+                   select s
+                   ).ToList();
+                return pictures;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+    }
+}
